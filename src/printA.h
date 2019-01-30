1bf8099bac7849da8e957dfaa43030fc4a003279
Aidy <aidyszh@gmail.com>
2019-01-30 15:38:19 +0800

    skip list and sorting summary

--- src/printA.h
+++ src/printA.h
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 static void printA(int *a, int n) {
     for (int i = 0; i < n; i++) {
         cout<<a[i] << " ";
-        if (i%20 == 0) cout << endl;
+        if (i != 0 && i%20 == 0) cout << endl;
     }
     cout << "\n";
 }
